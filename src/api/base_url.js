ad1dfb622623a865db86e7e2ac31fdd7807060da
Hari Gopal Sharma <harigopal@valere.io>
2023-04-13 20:33:38 +0530

    changed the environment to production

--- src/api/base_url.js
+++ src/api/base_url.js
@@ -1,7 +1,7 @@
 const base_uri = {
     // base_uri_admin: `http://44.195.125.80:9200/admin`
-    base_uri_admin: `http://20.245.131.160/api/v1`
-    // base_uri_admin: `https://teachers.sparkfamily.org/api/v1`
+    // base_uri_admin: `http://20.245.131.160/api/v1`
+    base_uri_admin: `https://teachers.sparkfamily.org/api/v1`
 
 
 
