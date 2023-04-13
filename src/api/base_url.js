20e644c338f79be754f3fbdd579a9c6c5e5a10da
Hari Gopal Sharma <harigopal@valere.io>
2023-04-13 16:26:00 +0530

    base url changed

--- src/api/base_url.js
+++ src/api/base_url.js
@@ -1,9 +1,8 @@
 const base_uri = {
     // base_uri_admin: `http://44.195.125.80:9200/admin`
-    // base_uri_admin: `http://20.245.131.160/api/v1`
-    base_uri_admin: `https://teachers.sparkfamily.org/api/v1`
-    // base_uri_admin: `http://192.168.178.243:5002/api/v1`,
-    // image_base_url: `http://192.168.178.243:5002`
+    base_uri_admin: `http://20.245.131.160/api/v1`
+    // base_uri_admin: `https://teachers.sparkfamily.org/api/v1`
+
 
 
 }
