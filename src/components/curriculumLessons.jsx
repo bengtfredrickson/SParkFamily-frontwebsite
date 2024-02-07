f01780c20588a45bbdf814360b4dcd004c8010cd
Hari Gopal Sharma <harigopal@valere.io>
2024-02-07 16:05:08 +0530

    merged

--- src/components/curriculumLessons.jsx
+++ src/components/curriculumLessons.jsx
@@ -865,16 +865,16 @@ export default function CurriculumoLessonPlans() {
     if (item.key_type === 1) {
       return (
         <tr key={index}>
-          <td style={{ border: '2px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.key}:</td>
-          <td style={{ border: '2px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.value}</td>
+          <td style={{ border: '1px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.key}:</td>
+          <td style={{ border: '1px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.value}</td>
         </tr>
       );
     }
     if (item.key_type === 2) {
       return (
         <tr key={index}>
-          <td style={{ border: '2px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.key}:</td>
-          <td style={{ border: '2px solid #dddddd', textAlign: 'left', padding: '8px' }}>
+          <td style={{ border: '1px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.key}:</td>
+          <td style={{ border: '1px solid #dddddd', textAlign: 'left', padding: '8px' }}>
             <img
               src={item.value}
               alt=""
@@ -887,8 +887,8 @@ export default function CurriculumoLessonPlans() {
     if (item.key_type === 3) {
       return (
         <tr key={index}>
-          <td style={{ border: '2px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.key}:</td>
-          <td style={{ border: '2px solid #dddddd', textAlign: 'left', padding: '8px' }} dangerouslySetInnerHTML={{ __html: item.value }}></td>
+          <td style={{ border: '1px solid #dddddd', textAlign: 'left', padding: '8px' }}>{item.key}:</td>
+          <td style={{ border: '1px solid #dddddd', textAlign: 'left', padding: '8px' }} dangerouslySetInnerHTML={{ __html: item.value }}></td>
         </tr>
       );
     }
